a0004591ba6cb79a7886a41300817988c04dc540
tototorio <diegoeoq@gmail.com>
2025-12-02 20:00:20 -0300

    Se comenzó a trabajar en consultas personalizadas

--- app/repositories/book.py
+++ app/repositories/book.py
@@ -1,7 +1,9 @@
 """Repository for Book database operations."""
+from typing import Sequence
 
 from advanced_alchemy.repository import SQLAlchemySyncRepository
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from app.models import Book
 
@@ -11,7 +13,15 @@ class BookRepository(SQLAlchemySyncRepository[Book]):
 
     model_type = Book
 
+    def get_avalibre_books(self) -> Sequence[Book]:
+        stmt = select(Book).where(Book.stock > 0)
+
+        result = self.session.execute(stmt)
+
+        return result.scalars().all()
+
 
 async def provide_book_repo(db_session: Session) -> BookRepository:
     """Provide book repository instance with auto-commit."""
     return BookRepository(session=db_session, auto_commit=True)
+
